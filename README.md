c7c6bb6e455df57fad3495eb4153e055c498b8af
Caio Guimaraes <caio.b.vilar@gmail.com>
2023-11-29 19:41:02 -0300

    ceedling and unity not working, something is missing.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nuttx-ddrivers
 Device Drivers Development on NuttX RTOS
+
+## Steps
+
+1. Build Image [done];
+2. Run a container from image;
+3. Added CMake;
+4. Added clang-tidy and clang-format;
+5. Ceedling and unity added, but not working.
---
+++ build/CMakeFiles/Dummy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dummy.dir/src/dummy.c.o"
+  "libDummy.a"
+  "libDummy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dummy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
